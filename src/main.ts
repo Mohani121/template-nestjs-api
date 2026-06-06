e28621addf2ec6f6069ce14e7bc505102eea918e
Mohani121 <mohaworks01@gmail.com>
2026-06-06 08:26:29 +0300

    feat: add global response interceptor and exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,31 @@
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
+import helmet from 'helmet'
 import { AppModule } from './app.module'
+import { HttpExceptionFilter } from '@/common/filters/http-exception.filter'
+import { ResponseInterceptor } from '@/common/interceptors/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   const config = app.get(ConfigService)
   const port = config.get<number>('PORT', 3000)
+
+  // Security
+  app.use(helmet())
+  app.enableCors()
+
+  // Global pipes, filters, interceptors
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
+  app.useGlobalFilters(new HttpExceptionFilter())
+  app.useGlobalInterceptors(new ResponseInterceptor())
 
   await app.listen(port)
   console.log(`🚀 Server running on http://localhost:${port}`)
